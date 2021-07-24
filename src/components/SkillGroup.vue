9b5076088a33675ec554bb79ab97fffdd19bf105
sky0621 <sky14work@gmail.com>
2021-07-24 18:16:57 +0900

    skill

---
+++ src/components/SkillGroup.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>SkillGroup</div>
+  <div>{{ skillGroups_ }}</div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, PropType, computed } from 'vue'
+  import { SkillGroup } from '@/types/skill'
+
+  export default defineComponent({
+    name: 'SkillGroupComponent',
+    props: {
+      skillGroups: {
+        type: Array as PropType<SkillGroup[]>,
+        default: undefined,
+      },
+    },
+    setup(props) {
+      const skillGroups_ = computed(() => {
+        if (!props || !props.skillGroups) return []
+        return props.skillGroups
+      })
+      return { skillGroups_ }
+    },
+  })
+</script>
+
+<style scoped lang="scss"></style>
--- src/pages/Skill.vue
+++ src/pages/Skill.vue
@@ -1,15 +1,29 @@
 <template>
-  <Skill />
+  <SkillGroupComponent :skill-groups="skillGroups" />
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue'
-  import Skill from '@/components/Skill.vue'
+  import { defineComponent, onMounted, ref } from 'vue'
+  import SkillGroupComponent from '@/components/SkillGroup.vue'
+  import { SkillService } from '@/service/SkillService'
 
   export default defineComponent({
     name: 'SkillPage',
     components: {
-      Skill,
+      SkillGroupComponent,
+    },
+    setup() {
+      const skillGroups = ref()
+
+      onMounted(async () => {
+        try {
+          skillGroups.value = await new SkillService().getSkillGroups()
+        } catch (e) {
+          console.error(e)
+        }
+      })
+
+      return { skillGroups }
     },
   })
 </script>
