47dbe87d1b82c4f747c4cf5847e57db7b3f193f7
sky0621 <sky14work@gmail.com>
2021-08-06 01:54:30 +0900

    backend

---
+++ admin/frontend/src/service/BasicService.ts
@@ -0,0 +1,5 @@
+export class BasicService {
+  async getBasicInfo(): string {
+    return ''
+  }
+}
--- admin/src/service/BasicService.ts
+++
@@ -1,7 +0,0 @@
-export class BasicService {
-  async getBasicInfo(): any {
-    const res = await prisma.basic.findMany()
-    console.log(res)
-    return res
-  }
-}
