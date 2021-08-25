601908c1c3eb3951c9d014884bdbc12023c84364
sky0621 <sky14work@gmail.com>
2021-08-26 00:56:00 +0900

    put: basic

--- admin/backend/src/index.ts
+++ admin/backend/src/index.ts
@@ -18,12 +18,30 @@ app.get('/basic', async (req, res) => {
 })
 
 app.get('/basic/:id', async (req, res) => {
-    console.log(req.params)
     const { id }: { id?: string } = req.params
     const basic = await prisma.basic.findUnique({
         where: { id: Number(id) },
     })
     res.json(basic)
+})
+
+app.put('/basic/:id', async (req, res) => {
+    const { id } = req.params
+    const { nickname, birthday, job, belongTo } = req.body
+    try {
+        const updatedBasic = await prisma.basic.update({
+            where: { id: Number(id) || undefined },
+            data: {
+                nickname: nickname,
+                birthday: birthday,
+                job: job,
+                belongTo: belongTo,
+            },
+        })
+        res.json(updatedBasic)
+    } catch (error) {
+        res.json({ error: `Basic with ID ${id} does not exist in the database` })
+    }
 })
 
 // ------------------------------------------------------------------
