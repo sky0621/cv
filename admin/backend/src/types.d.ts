de4f41fc324b5e18775bf719a5dce976b35edd31
sky0621 <sky14work@gmail.com>
2022-11-09 09:42:15 +0900

    continue

--- admin/backend/src/types.d.ts
+++
@@ -1,6 +0,0 @@
-import {Basic, Like, Note, NoteItem, Output, Qualification, Skill, SkillVersion, User} from "@prisma/client";
-
-export type UserModel = User | null
-export type BasicModel = Basic & { outputs: Output[], likes: Like[], qualifications: Qualification[] } | null
-export type NoteModel = Note & { items: NoteItem[] } | null
-export type SkillModel = Skill & { SkillVersion: SkillVersion[] } | null
