73a4d76ea30e4c35adee68e2708ca6e083466a2d
sky0621 <sky14work@gmail.com>
2023-04-27 22:35:30 +0900

    -

--- app/src/components/career/GroupPeriod.vue
+++ app/src/components/career/GroupPeriod.vue
@@ -1,39 +1,46 @@
 <script lang="ts" setup>
-import {computed, PropType} from 'vue'
-import {CalculationService} from '@/service/CalculationService'
-import {Career} from '/@/types/career'
+  import { computed, PropType } from 'vue'
+  import { CalculationService } from '@/service/CalculationService'
+  import { Career } from '/@/types/career'
 
-const props = defineProps({careers: Array as PropType<Career[]>})
+  const props = defineProps({ careers: Array as PropType<Career[]> })
 
-const len = props?.careers ? props.careers.length : 0
+  const len = props?.careers ? props.careers.length : 0
 
-const cs = new CalculationService()
+  const cs = new CalculationService()
 
-/*
- * TODO: このあたり、適当にやりすぎ。直す。
- */
-const from_ = computed(() => {
-  const firstCareer = props?.careers?.find((_, idx) => idx === len - 1)
-  if (!firstCareer) return {from: '?', to: '?'}
-  return cs.toStrYearMonth(firstCareer.from.year, firstCareer.from.month)
-})
+  const from_ = computed(() => {
+    const firstCareer = props?.careers?.find((_, idx) => idx === len - 1)
+    if (!firstCareer) return { from: '?', to: '?' }
+    return cs.toStrYearMonth(firstCareer.from.year, firstCareer.from.month)
+  })
 
-const to_ = computed(() => {
-  const latestCareer = props?.careers?.find((_, idx) => idx === 0)
-  if (!latestCareer) return {from: '?', to: '?'}
-  return cs.toStrYearMonth(latestCareer.to.year, latestCareer.to.month)
-})
+  const to_ = computed(() => {
+    const latestCareer = props?.careers?.find((_, idx) => idx === 0)
+    if (!latestCareer) return { from: '?', to: '?' }
+    return cs.toStrYearMonth(latestCareer.to.year, latestCareer.to.month)
+  })
 
-const df = computed(() => {
-  const firstCareer = props?.careers?.find((_, idx) => idx === len - 1)
-  if (!firstCareer) return '?'
-  const latestCareer = props?.careers?.find((_, idx) => idx === 0)
-  if (!latestCareer) return '?'
-  return cs.differenceStrInMonths(
-      new Date(`${latestCareer.to?.year}-${latestCareer.to?.month}-1`),
-      new Date(`${firstCareer.from?.year}-${firstCareer.from?.month}-1`)
-  )
-})
+  const df = computed(() => {
+    const firstCareer = props?.careers?.find((_, idx) => idx === len - 1)
+    if (!firstCareer) return '?'
+    const latestCareer = props?.careers?.find((_, idx) => idx === 0)
+    if (!latestCareer) return '?'
+    return cs.differenceStrInMonths(
+      new Date(
+        `${latestCareer.to?.year}-${latestCareer.to?.month}-1`.replace(
+          /-/g,
+          '/'
+        )
+      ),
+      new Date(
+        `${firstCareer.from?.year}-${firstCareer.from?.month}-1`.replace(
+          /-/g,
+          '/'
+        )
+      )
+    )
+  })
 </script>
 
 <template>
