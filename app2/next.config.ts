9ff599d2c7334099a7287850d289217123b6995f
sky0621 <sky14work@gmail.com>
2025-01-13 18:39:35 +0900

    -

--- app2/next.config.ts
+++ app2/next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  distDir: "../docs",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
--- app2/src/app/page.tsx
+++ app2/src/app/page.tsx
@@ -64,6 +64,19 @@ export default async function Page() {
         padding: "2rem",
       }}
     >
+      <header className="text-gray-400 bg-gray-900 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+            <span className="ml-3 text-xl">Tailblocks</span>
+          </a>
+          <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+            <a className="mr-5 hover:text-white">First Link</a>
+            <a className="mr-5 hover:text-white">Second Link</a>
+            <a className="mr-5 hover:text-white">Third Link</a>
+            <a className="mr-5 hover:text-white">Fourth Link</a>
+          </nav>
+        </div>
+      </header>
       <AboutSection
         attribute={attribute}
         activities={activities}
