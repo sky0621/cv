f8a5f1c026760a8311c4e33cc8c08c75c1a5126f
sky0621 <sky14work@gmail.com>
2025-01-15 23:06:13 +0900

    -

---
+++ app2/src/app/_components/Attribute/page.tsx
@@ -0,0 +1,3 @@
+export default function AttributePage() {
+  return <div>Attribute</div>;
+}
--- app2/src/app/_components/Dashboard.tsx
+++ app2/src/app/_components/Dashboard.tsx
@@ -18,9 +18,17 @@ import {
   Sheet,
   Tab,
   TabList,
+  TabPanel,
   Tabs,
 } from "@mui/joy";
 import AttributionIcon from "@mui/icons-material/Attribution";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import LaptopIcon from "@mui/icons-material/Laptop";
+import AttributePage from "@/app/_components/Attribute/page";
+import CareerPage from "@/app/_components/Career/page";
+import NotePage from "@/app/_components/Note/page";
+import SkillPage from "@/app/_components/Skill/page";
 
 type Props = {
   attribute: IAttribute | null;
@@ -38,19 +46,46 @@ export default function Dashboard(props: Props) {
         aria-label="Dashboard Tabs"
         orientation="horizontal"
         size="lg"
-        defaultValue={0}
+        defaultValue="attribute"
       >
-        <TabList disableUnderline sticky="top">
-          <Tab>
+        <TabList disableUnderline variant="soft" sticky="top">
+          <Tab value="attribute">
             <ListItemDecorator>
               <AttributionIcon />
             </ListItemDecorator>
             Attribute
           </Tab>
-          <Tab>Skill</Tab>
-          <Tab>Career</Tab>
-          <Tab>Note</Tab>
+          <Tab value="skill">
+            <ListItemDecorator>
+              <LaptopIcon />
+            </ListItemDecorator>
+            Skill
+          </Tab>
+          <Tab value="career">
+            <ListItemDecorator>
+              <EditNoteIcon />
+            </ListItemDecorator>
+            Career
+          </Tab>
+          <Tab value="note">
+            <ListItemDecorator>
+              <ErrorOutlineIcon />
+            </ListItemDecorator>
+            Note
+          </Tab>
         </TabList>
+        <TabPanel value="attribute">
+          <AttributePage />
+        </TabPanel>
+        <TabPanel value="skill">
+          <SkillPage />
+        </TabPanel>
+        <TabPanel value="career">
+          <CareerPage />
+        </TabPanel>
+        <TabPanel value="note">
+          <NotePage />
+        </TabPanel>
       </Tabs>
       <div>
         <Sheet variant="outlined">Welcome!</Sheet>
